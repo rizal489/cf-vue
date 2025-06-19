765d54d302dbf8b0c55340d0df0efd05d6c5451e
rizal489 <rizalharjo20@gmail.com>
2025-06-19 22:42:05 +0700

    POSENG

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { cloudflare } from '@cloudflare/vite-plugin
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
